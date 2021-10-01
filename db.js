6c1e29f5fd163abb2fd956909707b5f60eea17e0
Carter <carter531@icloud.com>
2021-09-30 22:38:49 -0700

    db mod

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const { Client } = require("pg");
 
 let DB_URI;
-
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///NNC_test";
+if (process.env.DATABASE_URL !== null) {
+  DB_URI = process.env.DATABASE_URL;
 } else {
   DB_URI = "postgresql:///NNC";
 }
